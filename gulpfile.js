3684accda41f0b2468ceae5afd50191770e865ca
Leandro Bezerra <leandrokw@gmail.com>
2020-04-20 13:14:04 -0300

    Config BrowserSync and change functions to arrow functions in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify-es').default,
     babel = require('gulp-babel'),
     concat = require('gulp-concat'),
-    newer = require('gulp-newer')
+    newer = require('gulp-newer'),
+    browserSync = require('browser-sync').create()
 
-let path = {
+const path = {
     src: {
         html: 'src/**/*.html',
-        images: 'src/images/**/*.*',
+        images: 'src/images/*',
         scss: 'src/scss/*.scss',
         js: 'src/js/**/*.js'
     },
@@ -18,41 +19,45 @@ let path = {
         css: 'public/assets/css/',
         js: 'public/assets/js/'
     },
-};
+}
 
-gulp.task('watch', () => {
-    gulp.watch([path.src.js], gulp.parallel('js'))
-    gulp.watch([path.src.scss], gulp.parallel('sass'))
-    gulp.watch([path.src.html], gulp.parallel('html'))
-    gulp.watch([path.src.images], gulp.parallel('images'))
-})
-
-gulp.task('html', () =>  
+const htmlWatch = () =>
     gulp.src(path.src.html)
         .pipe(newer(path.build.html))
         .pipe(gulp.dest(path.build.html))
-)
 
-gulp.task('images', () => 
+const imagesWatch = () =>
     gulp.src(path.src.images)
         .pipe(newer(path.build.images))
         .pipe(gulp.dest(path.build.images))
-)
 
-gulp.task('sass', () =>
+const sassWatch = () =>
     gulp.src(path.src.scss)
         .pipe(concat('app.min.css'))
         .pipe(sass({outputStyle: 'compressed'})
         .on('error', sass.logError))
         .pipe(gulp.dest(path.build.css))
-)
 
-gulp.task('js', () =>
+const jsWatch = () =>
     gulp.src([path.src.js])
         .pipe(babel({presets: ['env']}))
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(path.build.js))
-)
 
-gulp.task('default', gulp.series(['html', 'images', 'sass', 'js', 'watch']))
+const serverWatch = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./public",
+            index: "/index.html"
+        }
+    })
+
+    gulp.watch(path.src.html, htmlWatch)
+    gulp.watch(path.src.scss, sassWatch)
+    gulp.watch(path.src.js, jsWatch)
+    gulp.watch(path.src.images, imagesWatch)
+    gulp.watch("public/**/*.*").on('change', browserSync.reload)
+}
+
+gulp.task('default', gulp.parallel(htmlWatch, sassWatch, jsWatch, imagesWatch, serverWatch))
